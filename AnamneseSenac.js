be82f5d93c81007eb99bcdd4d95f8cde9ab3e993
William Correia <wburgard6@gmail.com>
2026-06-01 23:05:16 -0300

    add Inter font pack

--- AnamneseSenac.js
+++ AnamneseSenac.js
@@ -2,8 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Login from './screens/Login';
 import { style } from './styles';
+import { useFonts, Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
 
 export default function App() {
+
+		const [fontsLoaded] = useFonts({
+			Inter_400Regular,
+			Inter_500Medium,
+		})
+
+		if (!fontsLoaded) {
+			return null
+		}
+
   return (
     <View style={style.container}>
 	    <Login/>
