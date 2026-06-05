27e9ee801818ff95dd5eaf7cb1b03be9f41b619a
William Correia <wburgard6@gmail.com>
2026-06-05 18:48:31 -0300

    add functions for screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { style } from '../styles'
 
-export default function Home() {
+export function Evolucao() {
 	return(
 		<View style={style.container}>
-			<Text>Login Realizado!</Text>
+			<Text>Evoluções</Text>
+		</View>
+	)
+}
+
+export function Prescricao() {
+	return(
+		<View style={style.container}>
+			<Text>Prescrições</Text>
+		</View>
+	)
+}
+
+export function Perfil() {
+	return(
+		<View style={style.container}>
+			<Text>Perfil</Text>
 		</View>
 	)
 }
