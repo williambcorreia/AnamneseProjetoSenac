0f8990604c73a4f91a083f7cee37cef13cc2af09
William Correia <wburgard6@gmail.com>
2026-06-05 14:23:22 -0300

    add home screen

---
+++ screens/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { style } from '../styles'
+
+export default function Home() {
+	return(
+		<View style={style.container}>
+			<Text>Login Realizado!</Text>
+		</View>
+	)
+}
