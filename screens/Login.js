83604f61dbadefbd6b0dfe70990ecbe01a97f0ca
William Correia <wburgard6@gmail.com>
2026-06-04 22:34:37 -0300

    add db integration

--- screens/Login.js
+++ screens/Login.js
@@ -1,17 +1,72 @@
-import React, { useState } from "react";
-import { Text, Image, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Text, Image, View, Alert } from "react-native";
 import { style } from "../styles";
+import * as Crypt from 'expo-crypto'
 import Logo from "../assets/pharmacy.png";
 import Input from "../components/Input";
 import Button from "../components/Button";
+import Deploy from "../database/InitDB";
 
 export default function Login() {
 
+		const [user, setUser] = useState("")
+		const [password, setPassword] = useState("")
+		const [db, setDb] = useState(null)
+
 		const [hidePassword, setHidePassword] = useState(true);
+
 		const showPassword = () => {
 			setHidePassword(!hidePassword)
 		}
+
+	useEffect(() => {
+		async function connectDB() {
+			const connection = await Deploy()
+			if (connection === "err") {
+				Alert.alert("Erro", "Não foi possível se conectar ao banco de dados")
+			} else {
+				setDb(connection)
+			}
+		}
+		connectDB()
+	}, [])
 		
+
+	const handleLogin = async () => {
+		
+		if (!user || !password) {
+			Alert.alert("Erro", "Preencha todos os campos!")
+			return
+		}
+		
+		if (!db) {
+			Alert.alert("Erro", "Banco de dados ainda não carregou!")
+			return
+		}
+
+		try {
+			const passwordCrypto = await Crypt.digestStringAsync(
+				Crypt.CryptoDigestAlgorithm.SHA256, password
+			)
+
+			const userFound = await db.getFirstAsync(
+				'SELECT * FROM users WHERE user = ? AND password = ?;',
+				[user, passwordCrypto]
+			)
+
+			if (userFound) {
+				Alert.alert(`${userFound.user} ${userFound.role}`)
+			} else {
+				Alert.alert("USUARIO OU SENHA INCORRETO")
+			}
+		}
+		
+		catch (error) {
+			Alert.alert("NÃO LOGIN")
+		}
+
+	}
+
 	return(
 		<View style={style.container}>
 		  <View style={style.greenBorder}/>
@@ -22,18 +77,27 @@ export default function Login() {
       </View>
 
       <View style={style.loginMid}>
+
 				<Text style={style.f16r}>Usuário</Text>
-        <Input placeholder="Ex: joaosilva" iconName="user-alt"/>
+        <Input 
+					placeholder="Ex: joaosilva" 
+					iconName="user-alt" 
+					onChangeText={setUser} 
+					value={user}/>
+
 				<Text style={[style.f16r, {paddingTop: 10}]}>Senha</Text>
         <Input 
 					placeholder="Ex: 1234@MairA!" 
 					iconName={hidePassword ? "eye-slash" : "eye"}
 					iconPress={showPassword} 
-					secureTextEntry={hidePassword}/>
+					secureTextEntry={hidePassword}
+					value={password}
+					onChangeText={setPassword}/>
+
       </View>
 
       <View style={style.loginBottom}>
-				<Button title={"Entrar"}/>
+				<Button title={"Entrar"} onPress={handleLogin}/>
 				<Text style={style.f16r}>Não possui uma conta?
 					<Text style={[style.f16r, {color: 'dodgerblue'}]}> Solicitar</Text>
 				</Text>
