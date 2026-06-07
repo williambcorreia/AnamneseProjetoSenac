b7f93470e6a8191e5386fd5ecc30796f09250332
William Correia <wburgard6@gmail.com>
2026-06-06 23:49:19 -0300

    add functions for each screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,37 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Alert } from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { style } from '../styles'
+import Button from '../components/Button'
+import CfgButton from '../components/ConfigButton'
+import RoundBtn from '../components/RoundButton'
+import CloseBtn from '../components/CloseButton'
 
-export function Evolucao() {
+export function Evolucao({route, navigation}) {
+
+	const {role} = route.params || {}
+
 	return(
 		<View style={style.container}>
 			<Text>Evoluções</Text>
+			{ role === 'Enf' ? (
+				<RoundBtn iconName="plus" iconColor='white' onPress={() => navigation.navigate('Transcrição')}/>
+			) : null}
 		</View>
 	)
 }
 
-export function Prescricao() {
+export function Transcricao({route, navigation}) {
+	return(
+		<View style={style.container}>
+		<View style={style.greenBorder}/>
+			<Text>Transcricao</Text>
+			<CloseBtn iconName="chevron-left" onPress={() => navigation.goBack()}/>
+		</View>
+	)
+}
+
+export function Prescricao({route}) {
 	return(
 		<View style={style.container}>
 			<Text>Prescrições</Text>
@@ -19,10 +39,18 @@ export function Prescricao() {
 	)
 }
 
-export function Perfil() {
+export function Config({route, navigation}) {
+
+	const handleLogout = () => {
+		Alert.alert("Encerrar Sessão", "Deseja realmente sair da sua conta?",
+			[{ text: "Cancelar", style:'cancel'}, {text: "Sair", onPress: () => navigation.replace('Login')}],
+			{cancelable: true}
+		)
+	}
+
 	return(
-		<View style={style.container}>
-			<Text>Perfil</Text>
+		<View style={[style.container, {justifyContent: 'flex-start'}]}>
+			<CfgButton title="Sair" titleColor="red" onPress={handleLogout}/>
 		</View>
 	)
 }
