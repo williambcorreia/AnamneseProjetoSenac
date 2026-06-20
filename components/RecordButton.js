116046178df3b268bbadf7a786cdc18f06825b8f
William Correia <wburgard6@gmail.com>
2026-06-20 14:05:15 -0300

    add transcription function

--- components/RecordButton.js
+++ components/RecordButton.js
@@ -1,14 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { TouchableOpacity, Text, StyleSheet } from "react-native"; 
 import { FontAwesome5 } from "@expo/vector-icons";
 import { useSpeechRecognitionEvent, ExpoSpeechRecognitionModule } from "expo-speech-recognition";
 
-export default function RecordBtn({title, iconName, iconColor, iconSize, ...props}) {
+export default function RecordBtn({finalGravacao, title, iconName, iconColor, iconSize, ...props}) {
 
 	const [recOn, setRecOn] = useState(false)
+	const [textoVoz, setTextoVoz] = useState("")
+
+	useEffect(() => {
+		async function obterPermissao() {
+			const resposta = await ExpoSpeechRecognitionModule.requestPermissionsAsync();
+			if (!resposta.granted) {
+				console.warn("A permissão para usar o microfone foi negada pelo usuário.");
+			}
+		}
+		obterPermissao();
+	}, []);
+
+	useSpeechRecognitionEvent("start", () => {setRecOn(true)})
+	useSpeechRecognitionEvent("end", () => {
+
+		setRecOn(false)
+		if (textoVoz.trim().length > 0) {
+			finalGravacao(textoVoz)
+		}
+
+	})
+
+	useSpeechRecognitionEvent("result", (event) => {
+		const resultado = event.results[0]?.transcript
+		if (resultado) {
+			setTextoVoz(resultado)
+		}
+	})
+
+  const startRec = () => {
+		console.log("clique")
+    if (recOn) {
+      ExpoSpeechRecognitionModule.stop();
+    } else {
+      setTextoVoz("")
+
+		ExpoSpeechRecognitionModule.start({
+			lang: "pt-BR",
+			interimResults: true,
+			continuous: true,
+		})
+  }
+}
 
 	return(
-		<TouchableOpacity style={[style.button, recOn ? style.activeBtn : null]} onPress={() => setRecOn(!recOn)} {...props}>
+		<TouchableOpacity style={[style.button, recOn ? style.activeBtn : null]} onPress={startRec} {...props}>
 			<FontAwesome5 name={recOn ? "stop" : "microphone"} size={iconSize} color={iconColor}/>
 
 			{recOn ? 
@@ -37,4 +80,3 @@ export const style = StyleSheet.create({
 	},
 
 })
-
