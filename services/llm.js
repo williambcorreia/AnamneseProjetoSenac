a0726f27187c62ee801ac80abb02e3cf84d94828
William Correia <wburgard6@gmail.com>
2026-06-23 22:28:21 -0300

    update llm service for adb tunnel

--- services/llm.js
+++ services/llm.js
@@ -1,76 +1,31 @@
-import { initLlama } from 'llama.rn';
-import * as fs from 'expo-file-system/legacy';
-
-let motorLLM = null
-const nomeModelo = 'SmolLM2-360M-Instruct-Q4_K_M.gguf'; 
-const caminhoModelo = `${fs.documentDirectory}${nomeModelo}`;
-const urlModelo = `https://huggingface.co/bartowski/SmolLM2-360M-Instruct-GGUF/resolve/main/${nomeModelo}`;
-
-export async function baixarLLM() {
-
-
+export async function executarLLM(texto){
 	try {
-		const llmFile = await fs.getInfoAsync(caminhoModelo)
+		const resposta = await fetch("http://localhost:11434/api/generate", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				model: "qwen2.5:1.5b",
+				prompt: `Você é um enfermeiro especialista. Reescreva a evolução de enfermagem abaixo seguindo estas ordens:
+				- Corrija erros ortográficos e use termos técnicos formais da área.
+				- Mantenha estrita fidelidade aos fatos ditados, sem inventar dados.
+				- Formate o texto em tópicos Markdown simples (Ex: ### Item \\n informações).
+				- Responda APENAS com a evolução formatada, em português, sem introduções ou saudações.
+					Texto: "${texto}"`,
+				stream: false,
+				options: {
+					temperature: 0,
+					top_p: 0.9
+				}
+			}),
+		})
 
-		if (!llmFile.exists) {
+		const dados = await resposta.json()
 
-			console.log("Baixando LLM...")
-
-			const progressoDownload = (dados) => {
-					const progresso = ((dados.totalBytesWritten / dados.totalBytesExpectedToWrite) * 100).toFixed(1);
-					console.log(`Progresso do download: ${progresso}%`);
-			};
-
-			const resumableDownload = fs.createDownloadResumable(urlModelo, caminhoModelo, {}, progressoDownload)
-			const downloadResult = await resumableDownload.downloadAsync()
-			console.log("Download concluído!");
-
-		}
-
-		console.log("LLM já baixada...")
-
-		return
+		return dados.response
 
 	} catch (err) {
 		console.error("Erro: ", err)
-		throw err
 	}
-
 }
-
-export async function executarLLM(texto) {
-	try {
-		if (!motorLLM) {
-			motorLLM = await initLlama({
-				model: caminhoModelo,
-				n_ctx: 2048,
-				n_threads: 2
-			})
-		}
-
-    const prompt = 
-		`<|user|>
-		Você é um assistente médico brasileiro. Corrija a ortografia, pontue e organize o seguinte texto em tópicos e negritos usando Markdown. Responda apenas em português e envie apenas o texto final.
-
-		Texto para corrigir:
-		${texto}<|end|>
-		<|assistant|>`;
-
-		let llmLogText = ""
-    const resposta = await motorLLM.completion({
-      prompt: prompt,
-      temperature: 0,
-			onToken: (token) => {
-        llmLogText += token.token;
-        console.log(`[Gerando...] ${llmLogText}`); 
-    }
-    });
-
-    return resposta.text.trim();
-
-  } catch (error) {
-		console.error("Erro: ", error);
-    throw error;
-  }
-}
-
