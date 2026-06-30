76c7bddcef4515eec06f7da13753814c238fe278
William Correia <wburgard6@gmail.com>
2026-06-30 19:57:34 -0300

    project name definition

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# AnamneseProjetoSenac
-Projeto para o curso de Enfermagem do Senac sobre um app de anamnese via transcrição de áudio
+# EvolueAI
+
+EvolueAI é um aplicativo para transcrição de evolução e prescrição de enfermagem. Criado no Senac de Gravataí/RS pelo docente Luís Gustavo Pinheiro Lopes e o discente William Burgard Correia.
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { registerRootComponent } from 'expo';
 
-import Anamnese from './AnamneseSenac';
+import EvolueAI from './EvolueAI';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(Anamnese);
+registerRootComponent(EvolueAI);
