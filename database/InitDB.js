74b012bffc45c9fb5cbdbea738e6b79708ebaef0
William Correia <wburgard6@gmail.com>
2026-07-02 21:52:51 -0300

    add connect function, evolucoes table and username on users table

--- database/InitDB.js
+++ database/InitDB.js
@@ -1,18 +1,31 @@
 import * as SQLite from 'expo-sqlite';
 import * as Crypt from 'expo-crypto'
 
-export default async function DeployDB() {
+export async function DeployDB() {
 	
 	try {
-		const db = await SQLite.openDatabaseAsync('users.db')
+		const db = await SQLite.openDatabaseAsync('database.db')
+
+		await db.execAsync(`PRAGMA foreign_keys = ON;`)
 
 		await db.execAsync(`
 			CREATE TABLE IF NOT EXISTS users (
-				id INTEGER PRIMARY KEY AUTOINCREMENT,
-				user TEXT NOT NULL,
-				role TEXT NOT NULL,
-				email TEXT NOT NULL UNIQUE,
-				password TEXT NOT NULL
+				id_user INTEGER PRIMARY KEY AUTOINCREMENT,
+				user VARCHAR(100) NOT NULL,
+				username VARCHAR(100) NOT NULL,
+				role VARCHAR(50) NOT NULL,
+				email VARCHAR(50) NOT NULL UNIQUE,
+				password VARCHAR(64) NOT NULL
+			);
+		`)
+
+		await db.execAsync(`
+			CREATE TABLE IF NOT EXISTS evolucoes (
+				id_evolucao INTEGER PRIMARY KEY AUTOINCREMENT,
+				feito_por VARCHAR(100) NOT NULL,
+				nome_paciente VARCHAR(50) NOT NULL,
+				texto_evolucao TEXT NOT NULL,
+				data DATE NOT NULL,
 			);
 		`)
 
@@ -20,10 +33,10 @@ export default async function DeployDB() {
 
 		if (result.total === 0) {
 			const defaultUsers = [
-				{ user: "defenastradorVitao", role: "Root", email: "root@exemplo.com", password: "S3nh@R00T"},
-				{ user: "juliaSouza", role: "Enf", email: "enf@exemplo.com", password: "JuliaA45$"},
-				{ user: "amandaMendes", role: "TecEnf", email: "tecenf@exemplo.com", password: "AMANd#23%"},
-				{ user: "raimundoJorge", role: "Coord", email: "admin@exemplo.com", password: "rmndJG*09"}
+				{ user: "defenastradorVitao", username: "Vitor Cardoso", role: "Root", email: "root@exemplo.com", password: "S3nh@R00T"},
+				{ user: "juliaSouza", username: "Júlia Souza Rodrigues", role: "Enf", email: "enf@exemplo.com", password: "JuliaA45$"},
+				{ user: "amandaMendes", username: "Amanda Mendes Garcia", role: "TecEnf", email: "tecenf@exemplo.com", password: "AMANd#23%"},
+				{ user: "raimundoJorge", username: "Raimundo Jorge Montenegro", role: "Coord", email: "admin@exemplo.com", password: "rmndJG*09"}
 			]
 
 			for (const users of defaultUsers) {
@@ -31,8 +44,8 @@ export default async function DeployDB() {
 					Crypt.CryptoDigestAlgorithm.SHA256, users.password
 				)
 
-				await db.runAsync('INSERT INTO users (user, role, email, password) VALUES (?, ?, ?, ?);',
-				[users.user, users.role, users.email, passwordCrypto]
+				await db.runAsync('INSERT INTO users (user, username, role, email, password) VALUES (?, ?, ?, ?, ?);',
+				[users.user, users.username, users.role, users.email, passwordCrypto]
 				)
 			}
 	}
@@ -42,4 +55,14 @@ export default async function DeployDB() {
 	} catch (error) {
 		return "err"
 	}
+}
+
+export async function seedEvolucaoDB() {
+	try {
+		const db = await SQLite.openDatabaseAsync('database.db')
+	} catch (err) {
+		return "err"
+	}
+
+	return db
 }
