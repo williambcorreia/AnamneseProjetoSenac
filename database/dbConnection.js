182823e50bf480b25d7971ea3bb6ae3a756376aa
William Correia <wburgard6@gmail.com>
2026-07-20 20:41:32 -0300

    add connection pool file

---
+++ database/dbConnection.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv/config'
+import mysql from 'mysql2/promise'
+
+const pool = mysql.createPool({
+	host: process.env.DB_HOST,
+	port: Number(process.env.DB_PORT),
+	database: process.env.DATABASE,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	waitForConnections: true,
+	connectionLimit: 10,
+	queueLimit: 0
+})
+
+export default pool
