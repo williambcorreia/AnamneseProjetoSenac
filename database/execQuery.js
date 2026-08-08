39a77bf95ce261dc715d4d2d7550c055a5d2f496
William Correia <wburgard6@gmail.com>
2026-08-07 21:37:19 -0300

    change domain for permanent public ip

--- database/execQuery.js
+++ database/execQuery.js
@@ -1,10 +1,10 @@
 export default async function execQuery(query, qparams = []) {
 	try {
-		const response = await fetch('https://evolueaidbsenac.loca.lt/query', {
+		const response = await fetch('http://168.138.157.72:3000/query', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
-				'Bypass-Tunnel-Remainder': 'true'
+				'bypass-tunnel-reminder': 'true'
 			},
 			body: JSON.stringify({
 				sql: query,
@@ -13,7 +13,10 @@ export default async function execQuery(query, qparams = []) {
 		})
 
 		const text = await response.text()
-		if (!text) throw new Error(`Servidor retornou resposta vazia. (${response.status})`)
+		if (!text) throw new Error(response.status)
+
+		console.log(`Status HTTP: (${response.status})`)
+		console.log("Resposta bruta do servidor: ", text)
 
 		const data = JSON.parse(text)
 		return data
