1c27810b8569d728d76713833fea8a2708b56254
William Correia <wburgard6@gmail.com>
2026-08-07 22:23:49 -0300

    format output

--- database/execQuery.js
+++ database/execQuery.js
@@ -15,7 +15,7 @@ export default async function execQuery(query, qparams = []) {
 		const text = await response.text()
 		if (!text) throw new Error(response.status)
 
-		console.log(`Status HTTP: (${response.status})`)
+		console.log(`\nStatus HTTP: (${response.status})`)
 		console.log("Resposta bruta do servidor: ", text)
 
 		const data = JSON.parse(text)
