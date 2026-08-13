15f72c261c090d84c0418dffeb2bc26d2228ecb8
William Correia <wburgard6@gmail.com>
2026-08-12 22:05:06 -0300

    feat: card component for selecting anamnesis on Home

---
+++ components/Card.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+
+export default function CardEvolucao ({ item, onPress }) {
+
+	const dataFormatada = item?.criado_em 
+		? new Date(item.criado_em).toLocaleString('pt-BR')
+		: ""
+
+	return(
+		<TouchableOpacity style={style.card} onPress={onPress}>
+			<Text style={style.nome}>{item?.paciente || 'Erro ao carregar nome'}</Text>
+			<View style={style.separador}/>
+			<Text style={style.texto} numberOfLines={4}>{item?.texto_evolucao || 'Erro ao carregar evolução'}</Text>
+			<Text style={style.data}>{dataFormatada || 'Erro ao carregar data'}</Text>
+		</TouchableOpacity>
+	)
+}
+
+export const style = StyleSheet.create({
+
+	card: {
+		width: 330,
+		height: 150,
+		borderWidth: 2,
+		borderRadius: 30,
+		borderColor: 'gray',
+		paddingHorizontal: 10,
+		paddingVertical: 5
+	},
+
+	separador: {
+		width: '100%', 
+		height: 2, 
+		backgroundColor: 'gray'
+	},
+
+	nome: {
+    fontSize: 20,
+		fontFamily: 'Inter_500Medium',
+		marginBottom: 2
+	},
+
+	texto: {
+    fontSize: 15,
+		fontFamily: 'Inter_400Regular',
+		marginBottom: 2
+	},
+
+	data: {
+    fontSize: 15,
+		fontFamily: 'Inter_400Regular',
+		marginTop: 2
+	}
+})
